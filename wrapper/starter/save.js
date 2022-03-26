ec5a6258e1a60bfd5c808a3f0b5d078bd170353a
Joey Nigro <cujojoeynigro@gmail.com>
2022-03-26 12:33:05 -0500

    Fix importing

--- wrapper/starter/save.js
+++ wrapper/starter/save.js
@@ -1,22 +1,22 @@
-const loadPost = require("../misc/post_body");
-const starter = require("./main");
-const http = require("http");
-
-/**
- * @param {http.IncomingMessage} req
- * @param {http.ServerResponse} res
- * @param {import("url").UrlWithParsedQuery} url
- * @returns {boolean}
+/***
+ * starter save route
  */
-module.exports = function (req, res, url) {
-	if (req.method != "POST" || (url.path != "/goapi/saveTemplate/")) return;
-	loadPost(req, res).then(([data, mId]) => {
-		const trigAutosave = data.is_triggered_by_autosave;
-		if (trigAutosave && (!data.starterId || data.noAutosave)) return res.end("0");
+const starter = require("./main");
+const loadPost = require("../request/post_body");
 
+module.exports = function (req, res, url) {
+	if (req.method != "POST" || url.path != "/goapi/saveTemplate/") return;
+	loadPost(req, res).then(data => {
 		var body = Buffer.from(data.body_zip, "base64");
-		var thumb = data.thumbnail_large && Buffer.from(data.thumbnail_large, "base64");
-		starter.save(body, thumb, mId, data.presaveId).then((nId) => res.end("0" + nId));
+		var thumb = Buffer.from(data.thumbnail_large, "base64");
+		starter
+			.save(body, thumb, data.movieId || null)
+			.then(nId => res.end("0" + nId))
+			.catch(err => {
+				if (process.env.NODE_ENV == "dev") throw err;
+				console.error("Error saving starter: " + err)
+				res.end("1")
+			});
 	});
 	return true;
-};
+}
