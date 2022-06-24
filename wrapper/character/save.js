8c76ca4242384b793a72ef898fc7647da10f0bfe
Joey Nigro <cujojoeynigro@gmail.com>
2022-06-24 17:27:57 -0500

    Update save.js

--- wrapper/character/save.js
+++ wrapper/character/save.js
@@ -1,37 +1,66 @@
-const loadPost = require("../misc/post_body");
-const character = require("./main");
-const http = require("http");
+/**
+ * route
+ * character saving
+ */
+// stuff
+const { meta } = require("../asset/main");
+const Char = require("./main");
 
 /**
- * @param {http.IncomingMessage} req
- * @param {http.ServerResponse} res
+ * @param {import("http").IncomingMessage} req
+ * @param {import("http").ServerResponse} res
  * @param {import("url").UrlWithParsedQuery} url
  * @returns {boolean}
  */
-module.exports = function (req, res, url) {
-	if (req.method == "POST")
-		switch (url.pathname) {
-			case "/goapi/saveCCCharacter/":
-				loadPost(req, res).then(([data]) =>
-					character
-						.save(Buffer.from(data.body))
-						.then((id) => {
-							var thumb = Buffer.from(data.thumbdata, "base64");
-							character.saveThumb(thumb, id);
-							res.end(`0${id}`);
-						})
-						.catch(() => res.end(`10`))
-				);
-				return true;
+module.exports = async function (req, res, url) {
+	if (req.method != "POST") return;
 
-			case "/goapi/saveCCThumbs/":
-				loadPost(req, res).then(([data]) => {
-					var id = data.assetId;
-					var thumb = Buffer.from(data.thumbdata, "base64");
-					character.saveThumb(thumb, id);
-					res.end(`0${id}`);
-				});
+	switch (url.pathname) {
+		case "/goapi/saveCCCharacter/": { // save all
+			// check for missing data
+			if (!req.body.body || !req.body.thumbdata || !req.body.themeId) {
+				res.statusCode = 400;
+				res.end();
 				return true;
-		}
-	return;
-};
+			}
+			const body = Buffer.from(req.body.body);
+			const thumb = Buffer.from(req.body.thumbdata, "base64");
+
+			const meta = {
+				type: "char",
+				subtype: 0,
+				title: "Untitled",
+				themeId: req.body.themeId
+			};
+			try {
+				const id = Char.save(body, meta);
+				Char.saveThumb(id, thumb)
+				res.end("0" + id);
+			} catch (err) {
+				console.error("Error saving character:", err);
+				res.statusCode = 500;
+				res.end("1");
+			}
+			return true;
+		} case "/goapi/saveCCThumbs/": { // save thumbs
+			if (!req.body.thumbdata || !req.body.assetId) {
+				res.statusCode = 400;
+				res.end();
+				return true;
+			}
+			const cId = req.body.assetId;
+			const thumb = Buffer.from(req.body.thumbdata, "base64");
+
+			try {
+				meta(cId);
+				Char.saveThumb(cId, thumb)
+				res.end("0" + cId);
+			} catch (err) {
+				console.error("Error saving character thumb:", cId, err);
+				res.statusCode = 500;
+				res.end("1");
+			}
+			return true;
+		} default: return;
+	}
+}
