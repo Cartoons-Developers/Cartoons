dbd798cb92ea56800c60367fd8999c1a30203dad
Joey Nigro <cujojoeynigro@gmail.com>
2022-12-28 02:34:15 -0600

    Update main.js

--- wrapper/tts/main.js
+++ wrapper/tts/main.js
@@ -46,33 +46,6 @@ module.exports = function (voiceName, text) {
 				req.end();
 				break;
 			}
-			case 'cepstral':
-			case 'voiceforge': {
-				https.get('https://www.voiceforge.com/demo', r => {
-					const cookie = r.headers['set-cookie'];
-					var q = qs.encode({
-						voice: voice.arg,
-						voiceText: text,
-					});
-					var buffers = [];
-					var req = https.get({
-						host: 'www.voiceforge.com',
-						path: `/demos/createAudio.php?${q}`,
-						headers: { Cookie: cookie },
-						method: 'GET',
-					}, r => {
-						r.on('data', b => buffers.push(b));
-						r.on('end', () => {
-							const html = Buffer.concat(buffers);
-							const beg = html.indexOf('id="mp3Source" src="') + 20;
-							const end = html.indexOf('"', beg);
-							const loc = html.subarray(beg, end).toString();
-							get(`https://www.voiceforge.com${loc}`).then(res).catch(rej);
-						});
-					});
-				});
-				break;
-			}
 			case 'vocalware': {
 				var [eid, lid, vid] = voice.arg;
 				var cs = md5(`${eid}${lid}${vid}${text}1mp35883747uetivb9tb8108wfj`);
@@ -104,46 +77,6 @@ module.exports = function (voiceName, text) {
 						r.on("error", rej);
 					}
 				);
-				break;
-			}
-			case 'voicery': {
-				var q = qs.encode({
-					text: text,
-					speaker: voice.arg,
-					ssml: text.includes('<'),
-				});
-				https.get({
-					host: 'www.voicery.com',
-					path: `/api/generate?${q}`,
-				}, r => {
-					var buffers = [];
-					r.on('data', d => buffers.push(d));
-					r.on('end', () => res(Buffer.concat(buffers)));
-					r.on('error', rej);
-				});
-				break;
-			}
-			case 'watson': {
-				var q = qs.encode({
-					text: text,
-					voice: voice.arg,
-					download: true,
-					accept: "audio/mp3",
-				});
-				console.log(https.get({
-					host: 'text-to-speech-demo.ng.bluemix.net',
-					path: `/api/v1/synthesize?${q}`,
-					headers: {
-						Referer: 'https://www.vocalware.com/index/demo',
-						Origin: 'https://www.vocalware.com',
-						'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
-					},
-				}, r => {
-					var buffers = [];
-					r.on('data', d => buffers.push(d));
-					r.on('end', () => res(Buffer.concat(buffers)));
-					r.on('error', rej);
-				}));
 				break;
 			}
 		}
