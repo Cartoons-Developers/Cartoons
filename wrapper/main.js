61667807f9157eb4d1675a5cfeb5d273a54bc13f
Joey Nigro <cujojoeynigro@gmail.com>
2023-07-12 02:36:23 -0500

    Update main.js

--- wrapper/main.js
+++ wrapper/main.js
@@ -1,9 +1,1 @@
 require("./server");
-
-
-// Loads env.json for Wrapper version and build number
-const env = Object.assign(process.env,
-	require('./env'));
-// env.json variables
-let version = env.WRAPPER_VER;
-let build = env.WRAPPER_BLD;
