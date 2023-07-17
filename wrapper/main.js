c3d650d02f21c4ff220cd72a29fdc559f88d9807
Joey Nigro <cujojoeynigro@gmail.com>
2023-07-17 07:57:22 -0500

    Fixed

--- wrapper/main.js
+++ wrapper/main.js
@@ -1,38 +1,52 @@
-// Core part of Cartoons
-const RPC = require("discord-rpc");
-require("./server");
+/***
+ * Cartoons
+ */
+// assign config and env.json stuff to process.env
+const env = Object.assign(process.env, require("./env"), require("./config"));
+// modules
+const fs = require("fs");
+const path = require("path");
+// vars
+const assets = path.join(__dirname, env.ASSET_FOLDER);
+const cache = path.join(__dirname, env.CACHÉ_FOLDER);
+const saved = path.join(__dirname, env.SAVED_FOLDER);
 
+/**
+ * initialization
+ */
+// create directories if they're missing
+if (!fs.existsSync(assets)) fs.mkdirSync(assets);
+if (!fs.existsSync(cache)) fs.mkdirSync(cache);
+if (!fs.existsSync(saved)) fs.mkdirSync(saved);
+// start server
+const server = require("./server");
+server();
 
-// Loads env.json for Wrapper version and build number
-const env = Object.assign(process.env,
-	require('./env'));
-// env.json variables
-let version = env.WRAPPER_VER;
-let build = env.WRAPPER_BLD;
-
-
-// Discord rich presence
-const rpc = new RPC.Client({
-	transport: "ipc"
-});
-rpc.on("ready", () => {
-	// Sets RPC activity
-	rpc.setActivity({
-		// state: "Video List",
-		// disabled until automatic rpc status is done
-		details: "Version " + version +", build " + build,
-		startTimestamp: new Date(),
-		largeImageKey: "icon",
-		largeImageText: "Cartoons",
-		smallImageKey: "Cartoons",
-		smallImagetext: "Cartoons",
+/**
+ * rich presence
+ */
+if (process.env.DISCORD_RPC && process.env.DISCORD_RPC == "y") {
+	// get version number
+	const version = process.env.WRAPPER_VER;
+	const RPC = require("discord-rpc");
+	const rpc = new RPC.Client({
+		transport: "ipc"
 	});
-	// Logs "Rich presence is on!" in the console
-	console.log("Rich presence is on!")
-});
-// Connects RPC to app
-rpc.login({
-	clientId: "866340172874383370"
-}).catch(
-	console.log('RPC connection failed.')
-);
+
+	// sets rpc activity when ready
+	rpc.on("ready", () => {
+		rpc.setActivity({
+			details: `Version ${version}`,
+			startTimestamp: new Date(),
+			largeImageKey: "icon",
+			largeImageText: "Cartoons",
+			smallImageKey: "Cartoons",
+			smallImagetext: "Cartoons",
+		});
+	});
+
+	// connect rpc to app
+	rpc
+		.login({ clientId: "866340172874383370" })
+		.catch((e) => console.log("RPC connection failed."));
+}
