10e957068f5bc1ef3cb6dbbab2546f71330cf936
Joey Nigro <cujojoeynigro@gmail.com>
2023-12-18 06:31:51 -0600

    Update premade.js

--- wrapper/character/premade.js
+++ wrapper/character/premade.js
@@ -1,15 +1,5 @@
-const loadPost = require("../misc/post_body");
-const folder = process.env.PREMADE_FOLDER;
-const fs = require("fs");
-const http = require("http");
-
-/**
- * @param {http.IncomingMessage} req
- * @param {http.ServerResponse} res
- * @param {import("url").UrlWithParsedQuery} url
- * @returns {boolean}
- */module.exports = async function (req, res, url) {
+module.exports = async function (req, res, url) {
 	if (req.method != "POST" || url.pathname != "/goapi/getCCPreMadeCharacters") return;
 	res.end();
 	return true;
-};
+}
