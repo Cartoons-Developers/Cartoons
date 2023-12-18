ecdded46e680b3b88ea7b8269e728a6851202fd7
Joey Nigro <cujojoeynigro@gmail.com>
2023-12-18 01:53:40 -0600

    Update main.js

--- wrapper/main.js
+++ wrapper/main.js
@@ -1,38 +1,24 @@
-// Core part of Wrapper: Offline
-const RPC = require("discord-rpc");
-require("./server");
+/***
+ * Wrapper: Offline
+ */
+// assign config and env.json stuff to process.env
+const env = Object.assign(process.env, require("./env"), require("./config"));
+// modules
+const fs = require("fs");
+const path = require("path");
+// vars
+const assets = path.join(__dirname, env.ASSET_FOLDER);
+const cache = path.join(__dirname, env.CACHÉ_FOLDER);
+const saved = path.join(__dirname, env.SAVED_FOLDER);
+// stuff
+const server = require("./server");
 
-
-// Loads env.json for Wrapper version and build number
-const env = Object.assign(process.env,
-	require('./env'));
-// env.json variables
-let version = env.WRAPPER_VER;
-let build = env.WRAPPER_BLD;
-
-
-// Discord rich presence
-const rpc = new RPC.Client({
-	transport: "ipc"
-});
-rpc.on("ready", () => {
-	// Sets RPC activity
-	rpc.setActivity({
-		// state: "Video List",
-		// disabled until automatic rpc status is done
-		details: "Version " + version +", build " + build,
-		startTimestamp: new Date(),
-		largeImageKey: "icon",
-		largeImageText: "Wrapper: Offline",
-		smallImageKey: "Wrapper: Offline",
-		smallImagetext: "Wrapper: Offline",
-	});
-	// Logs "Rich presence is on!" in the console
-	console.log("Rich presence is on!")
-});
-// Connects RPC to app
-rpc.login({
-	clientId: "866340172874383370"
-}).catch(
-	console.log('RPC connection failed.')
-);
+/**
+ * initialization
+ */
+// create directories if they're missing
+if (!fs.existsSync(assets)) fs.mkdirSync(assets);
+if (!fs.existsSync(cache)) fs.mkdirSync(cache);
+if (!fs.existsSync(saved)) fs.mkdirSync(saved);
+// start server
+server();
